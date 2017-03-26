a37535e06e607b110a42b8a8548145c7c63226e5
Slava <rtootik@gmail.com>
2017-03-26 18:25:39 +0300

    lvl1

--- 12-HomeWork-UIViewGeometry/ViewController.m
+++ 12-HomeWork-UIViewGeometry/ViewController.m
@@ -17,8 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *createBlackField = [UIView new];
+    createBlackField = [self field:CGRectMake(16, 100, 288, 288) viewColor:[UIColor blackColor] viewParent:self.view];
+    UIView *createWiteField = [UIView new];
+    createWiteField = [self field:CGRectMake(17, 101, 286, 286) viewColor:[UIColor whiteColor] viewParent:self.view];
+    CGRect blackRect = CGRectMake(3, 3, 35, 35);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 4; j++) {
+            UIView *blackRectView = [UIView new];
+            blackRectView = [self field:blackRect viewColor:[UIColor blackColor] viewParent:createWiteField];
+            blackRect.origin.x +=70;
+        }
+        if (i % 2) {
+            blackRect.origin.x = 3;
+        } else {
+            blackRect.origin.x = 38;
+        }
+        blackRect.origin.y +=35;
+    }
 }
 
+- (UIView *) field:(CGRect) borderField viewColor:(UIColor *) fieldColor viewParent:(UIView *) fieldParent {
+    UIView *viewField = [[UIView alloc] initWithFrame:borderField];
+    viewField.backgroundColor = fieldColor;
+    [fieldParent addSubview:viewField];
+    viewField.autoresizingMask =    UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
+                                    UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    return viewField;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
