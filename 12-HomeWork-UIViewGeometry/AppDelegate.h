1bb22deb24b7df4f6974b21b78637f8d1b25153e
Slava <rtootik@gmail.com>
2017-03-26 17:08:44 +0300

    Initial Commit

---
+++ 12-HomeWork-UIViewGeometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  12-HomeWork-UIViewGeometry
+//
+//  Created by Slava on 26.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
