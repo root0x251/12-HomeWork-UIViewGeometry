046995bdb75f939fbcd96c3670ece4cf579a844b
Slava <rtootik@gmail.com>
2017-04-02 09:54:57 +0300

    3lvl

--- 12-HomeWork-UIViewGeometry/ViewController.m
+++ 12-HomeWork-UIViewGeometry/ViewController.m
@@ -10,23 +10,30 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSMutableArray *array;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    UIView *createBlackField = [UIView new];
-    createBlackField = [self field:CGRectMake(16, 100, 288, 288) viewColor:[UIColor blackColor] viewParent:self.view];
-    UIView *createWiteField = [UIView new];
-    createWiteField = [self field:CGRectMake(17, 101, 286, 286) viewColor:[UIColor whiteColor] viewParent:self.view];
+    
+    self.array = [NSMutableArray new];
+    
+    UIView *bigBlackRect = [UIView new];
+    bigBlackRect = [self field:CGRectMake(10, 100, 300, 300) colorBigRectView:[UIColor blackColor] viewParent:self.view];
+    UIView *bigWhiteRect = [UIView new];
+    bigWhiteRect = [self field:CGRectMake(12, 102, 296, 296) colorBigRectView:[UIColor whiteColor] viewParent:self.view];
+    
+    UIView *smallRect = [UIView new];
+    
     CGRect blackRect = CGRectMake(3, 3, 35, 35);
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 4; j++) {
-            UIView *blackRectView = [UIView new];
-            blackRectView = [self field:blackRect viewColor:[UIColor blackColor] viewParent:createWiteField];
+            smallRect = [self field:blackRect colorBigRectView:[UIColor blackColor] viewParent:bigWhiteRect];
             blackRect.origin.x +=70;
+            [self.array addObject:smallRect];
         }
         if (i % 2) {
             blackRect.origin.x = 3;
@@ -35,15 +42,38 @@
         }
         blackRect.origin.y +=35;
     }
+    
 }
 
-- (UIView *) field:(CGRect) borderField viewColor:(UIColor *) fieldColor viewParent:(UIView *) fieldParent {
+- (CGFloat) countFromZeroToOne {
+    return (float)(arc4random() % 256) / 255;
+}
+
+- (UIColor *) randomColor {
+    CGFloat r = [self countFromZeroToOne];
+    CGFloat g = [self countFromZeroToOne];
+    CGFloat b = [self countFromZeroToOne];
+    UIColor *color = [[UIColor alloc] initWithRed:r
+                                            green:g
+                                             blue:b
+                                            alpha:1];
+    return color;
+}
+
+- (UIView *) field:(CGRect) borderField colorBigRectView:(UIColor *) colorBigRect viewParent:(UIView *) fieldParent {
     UIView *viewField = [[UIView alloc] initWithFrame:borderField];
-    viewField.backgroundColor = fieldColor;
+    viewField.backgroundColor = colorBigRect;
     [fieldParent addSubview:viewField];
     viewField.autoresizingMask =    UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
-                                    UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
     return viewField;
+}
+
+- (void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    for (UIView *view in self.array) {
+        view.backgroundColor = [self randomColor];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,5 +81,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+
+
+//Студент
+//Доска должна быть вписана в максимально возможный квадрат, т.е. либо бока, либо верх или низ должны касаться границ экрана
+//Применяя соответствующие маски сделайте так, чтобы когда устройство меняет ориентацию, то все клетки растягивались соответственно и ничего не вылетало за пределы экрана.
+
+//Мастер
+//При повороте устройства все черные клетки должны менять цвет :)
+//Сделайте так, чтобы доска при поворотах всегда строго находилась по центру
+
+//Супермен
+//Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
+//После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
 
 @end
